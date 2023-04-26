7e7a30d87c0b4443f7c3b3cca5ccaf4fcd185ef6
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-04-26 13:25:37 +0300

    task3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,14 +11,12 @@ import transaction from '../Data/transactions.json';
 const App = () => {
   return (
     <div>
-      <h1>My Profile</h1>
       <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-        
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
